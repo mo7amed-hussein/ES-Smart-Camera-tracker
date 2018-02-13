c8b02498bb57ee84f9594f1d7abe9ae156838f2a
mohamed hussein <m.hussein1389@gmail.com>
2018-02-13 13:46:21 +0200

    create inital solution

---
+++ smart-camera/smart-camera.c
@@ -0,0 +1,17 @@
+/*
+ * smart_camera.c
+ *
+ * Created: 2/13/2018 1:43:42 PM
+ *  Author: mohamed
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
